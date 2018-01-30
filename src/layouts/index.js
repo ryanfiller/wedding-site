fa8be96a3ab7dee3c5b4cedfa35494581f8a83d3
Ryan Filler <=>
2018-01-30 12:16:48 -0600

    builds placeholder page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,27 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/header'
+import '../styles/styles.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Gabby & Ryan Are Getting Married"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="site">
       {children()}
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,23 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="site__invite invite">
+  
+    <div className="invite__image">
+      <img className="hover" src="../images/gabbyandryan-hover.jpg" />
+      <img src="../images/gabbyandryan.jpg" />
+    </div>
+  
+    <h1 className="invite__header">
+      Gabby & Ryan Are Getting Married!
+    </h1>
+
+    <main className="invite__body">
+      <p>
+        Check back as we finalize more details.
+      </p>
+    </main>
+
   </div>
 )
 
