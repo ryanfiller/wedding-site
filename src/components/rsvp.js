c1688ef97f94abaa2fd5dfde25fc530db7131b74
Ryan Filler <=>
2018-02-19 20:32:20 -0600

    basic form styles

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -1,4 +1,112 @@
 import React from "react";
+import styled from 'styled-components';
+import styles from '../config/styles.js';
+
+const Form = styled.form`
+    width: 100%;
+    max-width: 60rem;
+    box-sizing: border-box;
+    padding: 2rem;
+`
+
+const Row = styled.div`
+    display: flex;
+
+    margin-bottom: 2rem;
+
+    &:last-child {
+        margin-bottom: 0;
+    }
+
+    > * {
+        flex: 1;
+        margin-right: 2rem;
+
+        &:last-child {
+            margin-right: 0;
+        }
+    }
+`
+
+const Field = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 3rem;
+
+    input {
+        font-family: ${styles.sansSerif};
+        padding: .5rem;
+        border: 2px solid transparent;
+        border-radius: 0;
+        -webkit-appearence: none;
+        transition: .2s;
+        flex: 1;
+        order: 1;
+
+        &:focus {
+            outline: none;
+            border: 2px solid $color-gray;
+            margin: 0;
+
+            &::-webkit-input-placeholder {
+                transition: .2;
+                color: transparent;
+            }
+            &::-moz-placeholder {
+                transition: .2s;
+                color: transparent;
+            }
+            + label {
+                max-height: 2rem;
+            }
+        }
+    }
+
+    label {
+        max-height: 0;
+        font-weight: bold;
+        overflow: hidden;
+        font-size: .6em;
+        padding-left: 0.5rem;
+        flex: 1;
+        order: 2;
+        display: flex;
+        align-items: center;
+        transition: .2s;
+    }
+`
+
+const Radio = styled.div`
+    flex: 2;
+    div {
+        display: flex;
+        align-items: center;
+
+        input {
+            margin-right: 1rem;
+        }
+
+        label {
+            font-family: ${styles.sansSerif};
+            font-size: 1.25rem;
+            margin-right: 2rem;
+
+            &:last-child {
+                margin-right: 0;
+            }
+        }
+    }
+`
+
+const Dropdown = styled.div`
+    select {
+        height: 3rem;
+        width: 100%;
+        border-radius: 0;
+        border: none;
+        background-color: white;    
+    }
+`
 
 function encode(data) {
   return Object.keys(data)
@@ -31,7 +139,7 @@ export default class RSVP extends React.Component {
   render(state) {
     if(this.state.submitted != true) {
         return (
-            <form
+            <Form
                 id="contact-form"
                 name="RSVP"
                 method="post"
@@ -39,53 +147,60 @@ export default class RSVP extends React.Component {
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
-              >
+            >
 
-                  <div className="form__row">
-                      <input type="text" name="names" placeholder="Names *" required onChange={this.handleChange} />
-                      <label htmlFor="names">Names</label>
-                  </div>
+                <Row>
+                    <Field>
+                        <input type="text" name="names" placeholder="Names *" required onChange={this.handleChange} />
+                        <label htmlFor="names">Names</label>                                            
+                    </Field>
 
-                  <div className="form__row">
-                      <input type="email" name="email" placeholder="Email *" required onChange={this.handleChange} />
-                      <label htmlFor="email">Email</label>
-                  </div>
+                    <Field>
+                        <input type="email" name="email" placeholder="Email *" required onChange={this.handleChange} />
+                        <label htmlFor="email">Email</label>                                            
+                    </Field>
+                </Row>
 
-                  <div className="form__row">
-                    <div className="rsvp" required onChange={this.handleChange}>
-                      <input type="radio"
-                      name="rsvp" value="Attending" />
-                      <label htmlFor="rsvp1">Delightfully attending</label>
+                <Row>
+                    <Radio>
+                        <div className="rsvp" required onChange={this.handleChange}>
+                            <input type="radio"
+                            name="rsvp" value="Attending" />
+                            <label htmlFor="rsvp1">Delightfully<br/>attending</label>
 
-                      <input type="radio" 
-                      name="rsvp" value="Declining" />
-                      <label htmlFor="rsvp2">Regretfully declining</label>
-                    </div>
-                  </div>
+                            <input type="radio" 
+                            name="rsvp" value="Declining" />
+                            <label htmlFor="rsvp2">Regretfully<br/>declining</label>
+                        </div>
+                    </Radio>
 
-                  <div className="form__row">
-                      <select name="adults" required onChange={this.handleChange}>
-                        <option value="" disabled selected>Number of adults</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                      </select>
-                  </div>
+                    <Dropdown>
+                        <select name="adults" required onChange={this.handleChange}>
+                            <option value="" disabled selected>Number of adults</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                        </select>
+                    </Dropdown>
 
-                  <div className="form__row">
-                      <select name="children" required onChange={this.handleChange}>
-                        <option value="" disabled selected>Number of children</option>
-                        <option value="0">None</option>                        
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </select>
-                  </div>
+                    <Dropdown>
+                        <select name="children" required onChange={this.handleChange}>
+                            <option value="" disabled selected>Number of children</option>
+                            <option value="0">None</option>                        
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </Dropdown>
+                </Row>
 
-                  <div className="form__row">
-                      <input name="bot-field" hidden />
-                      <input className="button" type="submit" value="RSVP" />
-                  </div>
-              </form>
+                <Row>
+                    <Field>
+                        <input name="bot-field" hidden />
+                        <input className="button" type="submit" value="RSVP" />
+                    </Field>
+                </Row>
+
+              </Form>
           );
     } else {
         return(
